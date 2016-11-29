c372d90d2fed7714a8733702f1d774e67a55c38f
Prayten <denvyth@gmail.com>
2016-11-29 16:52:22 +0900

    Created new file For a test and added new function in lib.py

--- FirstPackage/lib.py
+++ FirstPackage/lib.py
@@ -7,3 +7,7 @@ def func2(param1, param2):
     a = 'param1 takes value ' + str(param1)
     b = 'param2 takes value ' + str(param2)
     print(a,'\n',b)
+
+
+def new_function():
+    print("YELLOW")
